54cf4d8e154433146aef3fc5d11d68493b320e18
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-02-23 00:56:09 +0530

    Maven Intro

---
+++ SeleniumPractice/src/Utilities/Constants.java
@@ -0,0 +1,7 @@
+package Utilities;
+
+public class Constants {
+    public static final String URL = "http://letskodeit.com";
+    public static final String File_Path = "C:\\Selenium\\Seleniumite\\SeleniumPractice\\src\\Data\\";
+    public static final String File_Name = "ExcelData.xlsx";
+}
