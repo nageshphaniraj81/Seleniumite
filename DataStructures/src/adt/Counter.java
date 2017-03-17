dcf6e60532356fe1b50bcf6140cb03724a8833d0
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-03-17 23:58:10 +0530

    Data structure adt

---
+++ DataStructures/src/adt/Counter.java
@@ -0,0 +1,25 @@
+package adt;
+
+public class Counter {
+	
+	private int count = 0;
+	private String str;
+	
+	public Counter(String str){
+		this.str = str;
+	}
+	
+	public void increment(){
+		count++;
+	}
+	
+	public int getCurrentValue(){
+		return count;
+	}
+	
+	public String toString(){
+		return "The value of the "+str+" counter is : "+count;
+	}
+	
+
+}
---
+++ DataStructures/src/adt/TestCounter.java
@@ -0,0 +1,17 @@
+package adt;
+
+public class TestCounter {
+
+	public static void main(String[] args) {
+		Counter count = new Counter("Test Counter");
+		count.increment();
+		count.increment();
+		System.out.println("Current value of the counter is "+count.getCurrentValue());
+        count.increment();
+        System.out.println(count.toString());
+		count.increment();
+		count.increment();
+		System.out.println("Current value of the counter is "+count.getCurrentValue());
+	}
+
+}
